71e9a78064148721c0927e13f9a186d87e780902
Honyzey <briac35.v@gmail.com>
2025-03-14 10:29:55 +0100

    Initial commit

---
+++ src/main/java/honyzey/powerofunivers/PowerOfUniversClient.java
@@ -0,0 +1,10 @@
+package honyzey.powerofunivers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PowerOfUniversClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/honyzey/powerofunivers/PowerOfUniversDataGenerator.java
@@ -0,0 +1,11 @@
+package honyzey.powerofunivers;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PowerOfUniversDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
