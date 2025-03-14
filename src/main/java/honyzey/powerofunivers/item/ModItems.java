4df88b5e42e14c55ba4bfd5c0e3452384559d28b
Honyzey <briac35.v@gmail.com>
2025-03-14 12:08:33 +0100

    First Item Added

--- src/main/java/honyzey/powerofunivers/item/ModItems.java
+++ src/main/java/honyzey/powerofunivers/item/ModItems.java
@@ -1,14 +1,16 @@
 package honyzey.powerofunivers.item;
 
 import honyzey.powerofunivers.PowerOfUnivers;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item Test = registerItem("test", new Item(new Item.Settings()));
+    public static final Item TEST_TEST = registerItem("test_test", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(PowerOfUnivers.MOD_ID, name), item);
@@ -17,5 +19,9 @@ public class ModItems {
     public static void registerItems() {
         // Register items here
         PowerOfUnivers.LOGGER.info("Registering Mod Items for " + PowerOfUnivers.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(TEST_TEST);
+        });
     }
 }
